5fff5a71877822b03d88b6eb89b1b832060ec467
chesha1 <chesha1@163.com>
2023-11-15 20:38:10 +0800

    update

--- blib/main.cpp
+++ blib/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include "test.h"
 #include <set>
+#include <map>
 
 using namespace std;
 
 
 int main() {
-    typedef std::_Rb_tree<key_type, value_type, _Identity<value_type>,
-            key_compare, _Key_alloc_type> _Rep_type;
+
 
 }
 
